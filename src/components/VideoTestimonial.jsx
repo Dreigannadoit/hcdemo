7e8f9b447b84290554cbf6c215f240356e8f01f7
Drei Abmab <dreiabmab@gmail.com>
2024-09-19 15:41:46 +0800

    first commit

--- src/components/VideoTestimonial.jsx
+++ src/components/VideoTestimonial.jsx
@@ -3,7 +3,11 @@ import { none } from '../constants'
 
 const VideoTestimonial = () => {
   return (
-    <div>{none}</div>
+    <>
+      <h1>{none}</h1>
+      
+      <a href="https://dreiabmab.com" target="_blank" rel="noopener noreferrer"></a>
+    </>
   )
 }
 
